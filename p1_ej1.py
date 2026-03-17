6e51621dd9aee0cb5767e03254610749ee6b10e1
matiasvga <vegamatiascorreo@gmail.com>
2026-03-17 11:38:29 -0300

    solucioné el ejercicio 1

---
+++ p1_ej1.py
@@ -0,0 +1,7 @@
+#Ejercicio 1 de la Práctica 1
+nacimiento = int(input("Ingrese su año de nacimiento:\n"))
+#print(nacimiento)
+print(f"Vas a cumplir 18 en el año {nacimiento+18}, 21 en el año {nacimiento+21}," 
+      + f"y 100 en el año {nacimiento+100}")
+
+#for i in range(3):
