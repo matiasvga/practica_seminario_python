e8172542a52aa2e8a8a5cfbb0802b1394c23fe29
unknown <vegamatiascorreo@gmail.com>
2026-04-28 17:47:28 -0300

    moví los ejercicios

---
+++ prueba.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.set_page_config(layout='wide')
+st.divider()
+with st.expander('Lista de compras'):
+    st.write('Papas')
+    st.write('Agua')
+st.write('probando')
+st.header('Header')
+st.subheader('Subheader')
+st.divider()
+st.title('Title')
